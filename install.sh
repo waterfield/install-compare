a923592bf7fa3745f15c176c080c1cc3a58b528c
Nathan Matthews <lowentropy@gmail.com>
2023-02-26 22:36:58 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,13 +14,13 @@ rm -rf $HOME/tmp/compare
 git clone git@github.com:waterfield/compare.git
 cd compare
 sudo docker build . -t compare
-echo << 'STR' > $HOME/bin/compare
+echo <<'END' > $HOME/bin/compare
 if [[ $1 == "update" ]]; then
   curl https://raw.githubusercontent.com/waterfield/install-compare/main/install.sh | bash
 else
   docker run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v `pwd`:/app/user compare $*
 fi
-STR
+END
 chmod +x $HOME/bin/compare
 cd $HOME
 rm -rf $HOME/tmp/compare
